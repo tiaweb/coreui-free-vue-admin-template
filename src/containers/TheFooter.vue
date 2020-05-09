233bf5d40bed802723ee7c85b666bd7f8b8b69c6
tiawebdemo <35865533+tiaweb@users.noreply.github.com>
2020-05-09 19:55:27 +0700

    Update TheFooter.vue

--- src/containers/TheFooter.vue
+++ src/containers/TheFooter.vue
@@ -1,13 +1,10 @@
 <template>
   <CFooter :fixed="false">
     <div>
-      <a href="https://coreui.io" target="_blank">CoreUI</a>
-      <span class="ml-1">&copy; {{new Date().getFullYear()}} creativeLabs.</span>
+      <a href="https://tiaweb.github.io" target="_blank">Tiaweb</a>
+      <span class="ml-1">&copy; {{new Date().getFullYear()}}Surabaya.</span>
     </div>
-    <div class="ml-auto">
-      <span class="mr-1">Powered by</span>
-      <a href="https://coreui.io/vue" target="_blank">CoreUI for Vue</a>
-    </div>
+    
   </CFooter>
 </template>
 
